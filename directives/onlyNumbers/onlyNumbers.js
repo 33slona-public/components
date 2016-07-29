ff8b050b4c386a3b7993e466bdba0047fcfe756e
Марат <marat@33slona.ru>
2016-07-29 17:03:16 +0300

    fix onlyNumbers

--- directives/onlyNumbers/onlyNumbers.js
+++ directives/onlyNumbers/onlyNumbers.js
@@ -2,10 +2,14 @@ slonComponents.directive('onlyNumbers', function() {
     return {
         require: 'ngModel',
         link: function (scope, element, attr, ngModel) {
+            var maxLen = attr['onlyNumbers']*1 || 16;
             ngModel.$parsers.push(function (inputValue) {
                 if (inputValue == undefined) return '';
                 var transformedInput = inputValue.replace(/[,\.]+/g, '.');
-                transformedInput = transformedInput.replace(/[^0-9+]/g, '');
+                if(transformedInput.length > maxLen) {
+                    transformedInput = transformedInput.slice(0, maxLen);
+                }
+                transformedInput = transformedInput.replace(/[^0-9]+/g, '');
                 var temp = transformedInput.split('.');
                 if (temp.length > 2) {
                     transformedInput = temp[0]+'.'+temp[1]+temp[2];
